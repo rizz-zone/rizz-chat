52d831a5fd7c989abce96ec2c6dc7eb6b2f0ad07
ezShroom <68300730+ezShroom@users.noreply.github.com>
2026-01-31 14:16:26 +0000

    move plugin to `ground0` repo

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,10 +2,15 @@ import devtoolsJson from 'vite-plugin-devtools-json'
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import { svelteKitWebSockets } from 'vite-plugin-sveltekit-websockets'
+import svelteKitWebSockets from 'vite-plugin-sveltekit-cf-websockets'
 
 export default defineConfig({
-	plugins: [tailwindcss(), svelteKitWebSockets(), sveltekit(), devtoolsJson()],
+	plugins: [
+		tailwindcss(),
+		svelteKitWebSockets({ verbose: true }),
+		sveltekit(),
+		devtoolsJson()
+	],
 	define: {
 		__WS_URL__: JSON.stringify(
 			process.env.PUBLIC_WS_URL ?? 'ws://localhost:5173/api/ws'
